c07ddac5fc16f80c297c7c81ee178fe2358eaea5
Aaron <610267308@qq.com>
2020-03-03 19:02:51 +0800

    39

--- 35. Search Insert Position.go
+++ 35. Search Insert Position.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 5))
-	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 2))
-	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 7))
-	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 0))
+	//fmt.Println(searchInsert([]int{1, 3, 5, 6}, 5))
+	//fmt.Println(searchInsert([]int{1, 3, 5, 6}, 2))
+	//fmt.Println(searchInsert([]int{1, 3, 5, 6}, 7))
+	//fmt.Println(searchInsert([]int{1, 3, 5, 6}, 0))
 
 }
 func searchInsert(nums []int, target int) int {
 
-	return i
+	return 0
 }
---
+++ 38.Count and Say.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i < 31; i++ {
+		fmt.Println(countAndSay(i))
+	}
+}
+func countAndSay(n int) string {
+	st := 1
+	for i := 0; i < n; i++ {
+
+	}
+	return ""
+}
