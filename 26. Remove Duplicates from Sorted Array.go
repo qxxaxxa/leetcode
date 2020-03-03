130a762590f8ba25e1c54944a5f6d8f7607fd5d9
Aaron <610267308@qq.com>
2020-03-03 11:28:10 +0800

    272727272727272727272727272727272727272727272727272727

--- 26. Remove Duplicates from Sorted Array.go
+++ 26. Remove Duplicates from Sorted Array.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
-func main() {
-
-	//fmt.Println(removeDuplicates(nil))
-	ints1 := []int{1, 1}
-	printMethod(ints1)
-	ints2 := []int{1,2}
-	printMethod(ints2)
-	ints3 := []int{1, 2, 3}
-	printMethod(ints3)
-
-	ints4 := []int{1, 1, 2}
-	printMethod(ints4)
-
-	ints5 := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
-	printMethod(ints5)
-
-}
+//func main() {
+//
+//	//fmt.Println(removeDuplicates(nil))
+//	ints1 := []int{1, 1}
+//	printMethod(ints1)
+//	ints2 := []int{1,2}
+//	printMethod(ints2)
+//	ints3 := []int{1, 2, 3}
+//	printMethod(ints3)
+//
+//	ints4 := []int{1, 1, 2}
+//	printMethod(ints4)
+//
+//	ints5 := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+//	printMethod(ints5)
+//
+//}
 
 func printMethod(ints1 []int) {
 	fmt.Println(removeDuplicates(ints1))
---
+++ 27. Remove Element.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	ints := []int{3, 2, 2, 3}
+	fmt.Println(removeElement(ints, 3))
+	fmt.Println(ints)
+
+	int1 := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	fmt.Println(removeElement(int1, 2))
+	fmt.Println(int1)
+
+}
+func removeElement(nums []int, val int) int {
+	if 0 == len(nums) {
+		return 0
+	}
+	index := 0
+	for _, num := range nums {
+		if num != val {
+			nums[index] = num
+			index++
+		}
+	}
+	return index
+}
