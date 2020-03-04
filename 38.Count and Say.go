f652013942b7624a935b4d8112485fbebd9c45e7
Aaron <610267308@qq.com>
2020-03-04 20:50:00 +0800

    iwati

--- 38.Count and Say.go
+++ 38.Count and Say.go
@@ -1,16 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	for i := 1; i < 31; i++ {
 		fmt.Println(countAndSay(i))
 	}
+	//fmt.Println(countAndSay(3))
 }
 func countAndSay(n int) string {
-	st := 1
-	for i := 0; i < n; i++ {
-
+	// 第一个1
+	buf := []byte{'1'}
+	for n > 1 {
+		buf = say(buf)
+		n--
 	}
-	return ""
+	return string(buf)
+}
+
+func say(buf []byte) []byte {
+	res := make([]byte, 0, len(buf)*2)
+	i, j := 0, 1
+	for i < len(buf) {
+		// 几个
+		for j < len(buf) && buf[j] == buf[i] {
+			j++
+		}
+		res = append(res, byte(j-i+'0'), buf[i])
+		i = j
+	}
+	return res
 }
