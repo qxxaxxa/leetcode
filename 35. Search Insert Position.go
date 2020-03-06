1458042954d508a51347dc583b8691a75608373c
Aaron <610267308@qq.com>
2020-03-07 01:46:35 +0800

    58

--- 35. Search Insert Position.go
+++ 35. Search Insert Position.go
@@ -1,12 +1,12 @@
 package main
 
-func main() {
-	//fmt.Println(searchInsert([]int{1, 3, 5, 6}, 5))
-	//fmt.Println(searchInsert([]int{1, 3, 5, 6}, 2))
-	//fmt.Println(searchInsert([]int{1, 3, 5, 6}, 7))
-	//fmt.Println(searchInsert([]int{1, 3, 5, 6}, 0))
-
-}
+//func main() {
+//	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 5))
+//	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 2))
+//	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 7))
+//	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 0))
+//
+//}
 func searchInsert(nums []int, target int) int {
 
 	return 0
